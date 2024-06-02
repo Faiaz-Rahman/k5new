64af0272f79731b97268a6f52d6d404f89424905
Faiaz-Rahman <faiazrahman70@gmail.com>
2024-06-02 16:40:04 +0600

    app level layout modified

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Head from './_components/Header'
+import Footer from './_components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -25,6 +26,7 @@ export default function RootLayout({
             <body className={inter.className}>
                 <Head />
                 {children}
+                <Footer />
             </body>
         </html>
     )
