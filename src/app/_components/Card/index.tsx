56a537eb9ea302083bcd9f21b531fb75b25e598d
Silicon <silicon@Silicons-MacBook-Pro.local>
2024-06-19 16:11:36 +0600

    card component added

---
+++ src/app/_components/Card/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+interface CardProps {
+    title: string
+}
+
+export default function Card({ title }: CardProps) {
+    return (
+        <div className="flex flex-col items-center">
+            <p
+                className="text-[20px] font-medium text-black
+                mb-10
+            "
+            >
+                {title}
+            </p>
+            <div className="h-[402px] w-[100%] bg-[--card] mb-16">
+                <div
+                    className="h-[50%] w-full flex items-center
+                justify-center bg-[--card-top] text-white
+                text-2xl
+            "
+                >
+                    By Grade
+                </div>
+                <div
+                    className="h-[50%] w-full flex items-center justify-center
+                text-2xl text-black
+            "
+                >
+                    By Topic
+                </div>
+            </div>
+        </div>
+    )
+}
