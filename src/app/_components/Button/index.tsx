31b9551acf6320d2d46920b4d8c0ede890ef07ee
Faiaz-Rahman <faiazrahman70@gmail.com>
2024-07-16 00:30:15 +0600

    button reusability

--- src/app/_components/Button/index.tsx
+++ src/app/_components/Button/index.tsx
@@ -3,22 +3,24 @@ import React from 'react'
 interface ButtonProps {
     title: string
     onPress: () => void
-    width: number
+    width?: number
+    wrapperStyle?: Object
+    wrapperTStyle?: string
 }
 
 export default function Button({
     title,
     onPress,
     width,
+    wrapperStyle,
+    wrapperTStyle,
 }: ButtonProps) {
     return (
         <button
-            className="h-10 px-6  bg-[--button-primary] rounded-full
-            font-bold text-xs
-        "
-            style={{
-                width,
-            }}
+            className={`h-10 px-6  bg-[--button-primary] 
+            rounded-full font-bold text-xs ${wrapperTStyle} 
+        `}
+            style={{ width, ...wrapperStyle }}
         >
             {title}
         </button>
