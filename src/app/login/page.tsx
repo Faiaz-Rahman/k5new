e94af8df17d7eb73f06650449d0d5ab4fd722ea4
Faiaz-Rahman <faiazrahman70@gmail.com>
2024-07-16 00:31:22 +0600

    login page ui added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,10 +2,15 @@
 
 import React, { useState } from 'react'
 import Input from '../_components/Input'
+import Button from '../_components/Button'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Login() {
     const [email, setEmail] = useState<string>('')
     const [pass, setPass] = useState<string>('')
+
+    const [showPass, setShowpass] = useState<boolean>(false)
 
     return (
         <div
@@ -33,10 +38,10 @@ export default function Login() {
                 placeholder="Email"
                 inputStyle="w-full text-[.8rem] h-10
                         outline-none border border-gray-200
-                        pl-4 rounded-md mt-2"
+                        pl-4 rounded-md"
                 labelStyle="text-[--login-text-color] font-normal
                 text-[.8rem] text-left"
-                wrapperStyle="w-3/6 mt-5"
+                wrapperStyle="w-4/6 mt-5"
                 // onChange={(e) => setEmail(e.target.value)}
             />
             <Input
@@ -44,12 +49,44 @@ export default function Login() {
                 type="password"
                 placeholder="Password"
                 inputStyle="w-full text-[.8rem] h-10
-                        outline-none border border-gray-200
-                        pl-4 rounded-md mt-2"
+                        outline-none 
+                        pl-4 rounded-md"
                 labelStyle="text-[--login-text-color] font-normal
                 text-[.8rem] text-left"
-                wrapperStyle="w-3/6 mt-3"
+                wrapperStyle="w-4/6 mt-3"
                 // onChange={(e) => setEmail(e.target.value)}
+            >
+                <div
+                    className="w-8 h-full flex
+                    items-center"
+                    onClick={() => {
+                        setShowpass((prev) => !prev)
+                    }}
+                >
+                    {!showPass ? (
+                        <FontAwesomeIcon
+                            icon={faEye}
+                            className="text-gray-400 text-[14px]"
+                        />
+                    ) : (
+                        <FontAwesomeIcon
+                            icon={faEyeSlash}
+                            className="text-gray-400 text-[14px]"
+                        />
+                    )}
+                </div>
+            </Input>
+
+            <Button
+                // passing tailwind styles
+                wrapperTStyle={'rounded-md w-4/6'}
+                wrapperStyle={{
+                    marginTop: 25,
+                }}
+                title="Log In"
+                onPress={() => {
+                    console.log('Log In')
+                }}
             />
         </div>
     )
