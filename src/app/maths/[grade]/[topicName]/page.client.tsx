13f63a3affa0669a4995b15e5512bd5ab35ca5b3
oscar-mike077 <faiazrahman70@gmail.com>
2024-11-16 17:41:23 +0600

    formatting the breadcrumbs (and getting params dynamically =>)

--- src/app/maths/[grade]/[topicName]/page.client.tsx
+++ src/app/maths/[grade]/[topicName]/page.client.tsx
@@ -8,12 +8,29 @@ export default function TopicWiseMathClient({
         topicName: string
     }
 }) {
+    const formattedGrade =
+        params.grade.charAt(0).toUpperCase() + params.grade.slice(1)
+
+    const formattedTopicName = params.topicName.split('_')
+
     return (
-        <div className="w-full bg-red-50 min-h-screen">
-            <p>
-                Topic wise math client with {params.grade} and{' '}
-                {params.topicName}{' '}
-            </p>
-        </div>
+        <>
+            <div className="py-2">
+                <p className="font-medium text-[10px]">
+                    Maths {' > '} {formattedGrade} {' > '}{' '}
+                    {formattedTopicName.map((item, index) => {
+                        return item + ' '
+                    })}
+                </p>
+            </div>
+            <div
+                className="py-2 mb-3 w-full
+            "
+            >
+                <p className="font-bold text-2xl">
+                    Grade 1 Number Pattern Worksheets
+                </p>
+            </div>
+        </>
     )
 }
