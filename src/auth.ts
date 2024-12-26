0fdbf412bcc8026262871df58beb40b0dc416800
Mohammad Fiaz Rahman Nafee <faiazrahman70@gmail.com>
2024-12-26 08:04:20 +0600

    oauth removed

--- src/auth.ts
+++ src/auth.ts
@@ -4,6 +4,3 @@ import Google from 'next-auth/providers/google'
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [Google],
 })
-
-// 173129752049-h3lv4mvi3tq6u415ee9c17fg08m1ds37.apps.googleusercontent.com
-// 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
