eba70542475625e99d76c1627bae9b6b59cb91be
Faiaz-Rahman <faiazrahman70@gmail.com>
2025-02-28 09:07:55 +0600

    full stripe subscription flow added

--- src/app/subscription/page.tsx
+++ src/app/subscription/page.tsx
@@ -21,6 +21,9 @@ import {
     DialogTitle,
     DialogTrigger,
 } from '@/components/ui/dialog'
+import LoadingUI from '../loading'
+import { useDispatch } from 'react-redux'
+import { updateSubscriptionStatus } from '@/lib/slices/authSlice'
 
 const stripePromise = loadStripe(
     process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY ?? ''
@@ -41,6 +44,9 @@ export default function Subscription() {
     const [plans, setPlans] = useState<Array<subscriptionPlanType>>(
         []
     )
+    const [hasLoaded, setHasLoaded] = useState<boolean>(false)
+    const dispatch = useDispatch()
+
     const [selectedPlan, setSelectedPlan] =
         useState<subscriptionPlanType>()
 
@@ -63,6 +69,7 @@ export default function Subscription() {
             )
 
             setPlans(respJsonData)
+            setHasLoaded(true)
         }
     }
 
@@ -70,6 +77,7 @@ export default function Subscription() {
         const stripe = await stripePromise
 
         const sessionIdResp = await fetch('/api/checkout-session', {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
@@ -80,7 +88,9 @@ export default function Subscription() {
 
         console.log('the session id =>', sessionId)
 
-        const resp = await stripe?.redirectToCheckout({ sessionId })
+        const resp = await stripe?.redirectToCheckout({
+            sessionId: sessionId.sessionId as string,
+        })
 
         if (resp?.error) {
             console.log('got error while redirecting to checkout')
@@ -94,6 +104,9 @@ export default function Subscription() {
             hasFetchedData.current = true
         }
     }, [])
+    if (!hasLoaded) {
+        return <LoadingUI />
+    }
 
     return (
         <div
@@ -184,6 +197,7 @@ export default function Subscription() {
                                 onClick={() => {
                                     setShowDialog(false)
                                 }}
+                                className="bg-white hover:bg-gray-100 border-2 border-black text-black"
                             >
                                 Cancel
                             </Button>
@@ -192,6 +206,21 @@ export default function Subscription() {
                                 onClick={() => {
                                     // @here
                                     // subscription logic
+                                    handleSubscription()
+
+                                    if (selectedPlan) {
+                                        dispatch(
+                                            updateSubscriptionStatus({
+                                                isSubscribed: true,
+                                                plan_name:
+                                                    selectedPlan?.name,
+                                                plan_price: `${(
+                                                    selectedPlan?.price /
+                                                    100
+                                                ).toFixed(2)}`,
+                                            })
+                                        )
+                                    }
                                 }}
                             >
                                 Purchase
